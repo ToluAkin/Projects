aeb5f182138d772c3e818e5a57be6e439dc1c785
ToluAkin <toluakintoye@gmail.com>
2018-12-11 16:24:52 +0100

    save to text deleted

--- proceeds.php
+++ proceeds.php
@@ -6,15 +6,7 @@
     $title = $_POST['title'];
     $note = $_POST['note'];  
     saveToDatabase($userid, $title, $note, $conn);
-    saveToFile($userid, $title, $note);
     header('Location:save.php');
-
-    function saveToFile($userid, $title, $note) {   
-        $fileHandler = fopen('entries.txt', 'a');   
-        $string = $title . ',' . $note . "\n";   
-        fwrite($fileHandler, $string);   
-        fclose($fileHandler); 
-    }
 
     function saveToDatabase($userid, $title, $note, $conn) {
         $sql = "INSERT INTO notes (userid, title, note, created_at)  VALUES ('$userid','$title','$note', NOW())";  
